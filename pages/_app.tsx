c80f726469ccb55533ac750e7e33a9d3c666c208
Wallace Júnior <wflj1997@hotmail.com>
2022-04-30 13:45:21 -0300

    initial ajustments

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,21 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app";
+import Head from "next/head";
+import { Fragment } from "react";
+import "../styles/globals.css";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
+const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
+  return (
+    <Fragment>
+      <Head>
+        <title>Netflix Clone</title>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
 
-export default MyApp
+      <Component {...pageProps} />
+    </Fragment>
+  );
+};
+
+export default MyApp;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,10 @@
 import type { NextPage } from "next";
-import Head from "next/head";
 
 const Home: NextPage = () => {
   return (
-    <div>
-      <Head>
-        <title>Netflix Clone</title>
-        <meta name="description" content="Netflix Clone" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main>
-        <h1 className="text-4xl">Hello world</h1>
-      </main>
-    </div>
+    <main>
+      <h1 className="text-4xl">Hello world</h1>
+    </main>
   );
 };
 
