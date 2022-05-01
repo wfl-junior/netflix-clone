cec17b6e4e8d54d6c33a14a14b3b0b30c738179a
Wallace Júnior <wflj1997@hotmail.com>
2022-05-01 11:22:07 -0300

    add path alias for absolute imports in tsconfig

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,7 @@
+import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import Head from "next/head";
 import { Fragment } from "react";
-import "../styles/globals.css";
 
 const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
   return (
