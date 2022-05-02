438d6cc8180cd08449468068f68d463e89da61e6
Wallace Júnior <wflj1997@hotmail.com>
2022-05-02 14:03:30 -0300

    fix styling for header notifications

--- components/Header/Notifications.tsx
+++ components/Header/Notifications.tsx
@@ -20,7 +20,7 @@ export const Notifications: React.FC = () => {
         leaveFrom="scale-100 opacity-100"
         leaveTo="scale-95 opacity-0"
       >
-        <Popover.Panel className="absolute top-full right-0 z-40 w-60 translate-y-[20%] transform xs:w-80 sm:w-96">
+        <Popover.Panel className="absolute top-full right-0 z-40 w-60 translate-y-6 transform xs:w-80 sm:w-96">
           <div className="absolute -top-2.5 right-0.5 -z-10 aspect-square w-3 text-white/75 xs:right-0 xs:-top-3.5 xs:w-4">
             <FontAwesomeIcon icon={faCaretUp} />
           </div>
@@ -28,7 +28,7 @@ export const Notifications: React.FC = () => {
           <ul className="flex w-full flex-col border border-white/25 bg-black/75 before:absolute before:inset-x-0 before:top-0 before:rounded before:border-t-2 before:border-white/75">
             <li>
               <Link href="#">
-                <a className="flex gap-5 bg-black/75 p-4 transition-colors duration-300 hover:bg-black">
+                <a className="flex gap-5 p-4 transition-colors duration-300 hover:bg-black">
                   <div className="flex w-1/2 items-center justify-center ">
                     <div className="relative z-10 before:absolute before:inset-0 before:-z-10 before:rounded before:bg-[#c2c2be] before:shadow after:absolute after:inset-0 after:-z-20 after:translate-x-1 after:translate-y-1 after:transform after:rounded after:bg-[#dbdbd7] after:shadow">
                       <img
